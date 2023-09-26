f24679b9126935df65ada35f61018cfd3e29d2b4
Nasibali144 <abdiyevnasibali@gmail.com>
2023-09-26 17:56:27 +0500

    commit 12

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:firebase_connection_1/blocs/post/post_bloc.dart';
 import 'package:firebase_connection_1/models/post_model.dart';
+import 'package:firebase_connection_1/services/auth_service.dart';
 import 'package:firebase_connection_1/services/db_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -38,17 +39,105 @@ class PostPage extends StatelessWidget {
               Post current = snapshot.hasData
                   ? Post.fromJson(
                       jsonDecode(jsonEncode(snapshot.data!.snapshot.value))
-                          as Map<String, Object?>)
+                          as Map<String, Object?>, isMe: AuthService.user.uid == post.userId)
                   : post;
 
               post.comments = current.comments;
               return ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+
                 shrinkWrap: true,
                 itemCount: current.comments.length,
                 itemBuilder: (context, index) {
                   final msg = current.comments[index];
-                  return ListTile(
-                    title: Text(msg.message),
+
+
+                  if(AuthService.user.uid == msg.userId) {
+                    return Row(
+                    children: [
+                      const Expanded(flex: 1, child: SizedBox.shrink(),),
+                      Expanded(flex: 3, child: Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.symmetric(vertical: 5),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(msg.message, style: Theme.of(context).textTheme.titleMedium,),
+                            const SizedBox(height: 5),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text("${msg.writtenAt.hour.toString().padLeft(2, "0")} : ${msg.writtenAt.minute.toString().padLeft(2, "0")}", style: Theme.of(context).textTheme.bodyLarge,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(msg.username, style: Theme.of(context).textTheme.headlineSmall,),
+                                    const SizedBox(width: 10),
+                                    CircleAvatar(
+                                      radius: 20,
+                                      backgroundColor: Colors.deepPurple,
+                                      child: Text(msg.username.substring(0, 1), style: Theme.of(context).textTheme.headlineLarge,),
+                                    ),
+                                  ],
+                                ),
+
+                              ],
+                            )
+
+                          ],
+                        ),
+                      ),),
+                    ],
+                  );
+                  }
+
+
+                  return Row(
+                    children: [
+                      Expanded(flex: 3, child: Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.symmetric(vertical: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(msg.message, style: Theme.of(context).textTheme.titleMedium,),
+                            const SizedBox(height: 5),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 20,
+                                      backgroundColor: Colors.lightBlueAccent,
+                                      child: Text(msg.username.substring(0, 1), style: Theme.of(context).textTheme.headlineLarge,),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Text(msg.username, style: Theme.of(context).textTheme.headlineSmall,)
+                                  ],
+                                ),
+                                Text("${msg.writtenAt.hour.toString().padLeft(2, "0")} : ${msg.writtenAt.minute.toString().padLeft(2, "0")}", style: Theme.of(context).textTheme.bodyLarge,)
+                              ],
+                            )
+
+                          ],
+                        ),
+                      ),),
+                      const Expanded(flex: 1, child: SizedBox.shrink(),),
+                    ],
                   );
                 },
               );
